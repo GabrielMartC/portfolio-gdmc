ce8b7e879aa22080e06c141b0fcd3aad2f08415e
GabrielM <gabrielmsistemas@gmail.com>
2025-06-03 01:52:43 -0300

    UPDATE styles responsive design: navbar, main, proyects (all with details) / FEATURE scripts: animation navbar

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
-function showEmail() {
-    const emailContainer = document.getElementById('emailContainer');
-    emailContainer.style.display = emailContainer.style.display === 'none' ? 'block' : 'none';
-}
+// function showEmail() {
+//     const emailContainer = document.getElementById('emailContainer');
+//     emailContainer.style.display = emailContainer.style.display === 'none' ? 'block' : 'none';
+// }
 
-function copyEmail() {
-    const emailText = document.getElementById('emailText').textContent;
-    navigator.clipboard.writeText(emailText).then(() => {
-        alert('¡Email copiado al portapapeles!');
-    }).catch(err => {
-        console.error('Error al copiar: ', err);
-    });
-}
+// function copyEmail() {
+//     const emailText = document.getElementById('emailText').textContent;
+//     navigator.clipboard.writeText(emailText).then(() => {
+//         alert('¡Email copiado al portapapeles!');
+//     }).catch(err => {
+//         console.error('Error al copiar: ', err);
+//     });
+// }
+
+const toggleButton = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
