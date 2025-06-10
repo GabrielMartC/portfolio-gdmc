67d89ae2ddfb95eeb511b5adb9317be2838147da
GabrielM <gabrielmsistemas@gmail.com>
2025-06-10 01:43:44 -0300

    style MOVILE finish/ html: add technologies in proyects, cv button

--- scripts.js
+++ scripts.js
@@ -1,20 +1,15 @@
-// function showEmail() {
-//     const emailContainer = document.getElementById('emailContainer');
-//     emailContainer.style.display = emailContainer.style.display === 'none' ? 'block' : 'none';
-// }
-
-// function copyEmail() {
-//     const emailText = document.getElementById('emailText').textContent;
-//     navigator.clipboard.writeText(emailText).then(() => {
-//         alert('¡Email copiado al portapapeles!');
-//     }).catch(err => {
-//         console.error('Error al copiar: ', err);
-//     });
-// }
-
+//navbar desplegable
 const toggleButton = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
 
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 })
+
+//oculta el navbar al hacer click en una seccion
+const navItems = document.querySelectorAll('#nav-links a');
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
