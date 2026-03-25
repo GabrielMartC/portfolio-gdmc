3f804990bd334067b0ed2a7890e82b83d78c4d91
GabrielM <gabrielmsistemas@gmail.com>
2026-03-25 18:53:27 -0300

    cambios nuevamente

--- scripts.js
+++ scripts.js
@@ -14,30 +14,61 @@ navItems.forEach(item => {
     });
 });
 
-//cambiar color icono
-// Email
-const emailIcon = document.getElementById("email-icon");
-emailIcon.addEventListener("mouseover", () => {
-  emailIcon.src = "./icons/email_cy2.png";  
-});
-emailIcon.addEventListener("mouseout", () => {
-  emailIcon.src = "./icons/email_w.png";  
+// //cambiar color icono
+// // Email
+// const emailIcon = document.getElementById("email-icon");
+// emailIcon.addEventListener("mouseover", () => {
+//   emailIcon.src = "./icons/email_cy2.png";  
+// });
+// emailIcon.addEventListener("mouseout", () => {
+//   emailIcon.src = "./icons/email_w.png";  
+// });
+
+// // LinkedIn
+// const linkedinIcon = document.getElementById("linkedin-icon");
+// linkedinIcon.addEventListener("mouseover", () => {
+//   linkedinIcon.src = "./icons/linkedin_cy2.png";
+// });
+// linkedinIcon.addEventListener("mouseout", () => {
+//   linkedinIcon.src = "./icons/linkedin_w.png";
+// });
+
+// // GitHub
+// const githubIcon = document.getElementById("github-icon");
+// githubIcon.addEventListener("mouseover", () => {
+//   githubIcon.src = "./icons/github_cy2.png";
+// });
+// githubIcon.addEventListener("mouseout", () => {
+//   githubIcon.src = "./icons/github_w.png";
+// });
+
+//about me
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(t => t.classList.remove("active"));
+    contents.forEach(c => c.classList.remove("active"));
+
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.target).classList.add("active");
+  });
 });
 
-// LinkedIn
-const linkedinIcon = document.getElementById("linkedin-icon");
-linkedinIcon.addEventListener("mouseover", () => {
-  linkedinIcon.src = "./icons/linkedin_cy2.png";
-});
-linkedinIcon.addEventListener("mouseout", () => {
-  linkedinIcon.src = "./icons/linkedin_w.png";
-});
+//copy email to clipb
+function copyEmail() {
+  const emailText = document.getElementById("mailto").innerText;
+  const notification = document.getElementById("copy-notification");
 
-// GitHub
-const githubIcon = document.getElementById("github-icon");
-githubIcon.addEventListener("mouseover", () => {
-  githubIcon.src = "./icons/github_cy2.png";
-});
-githubIcon.addEventListener("mouseout", () => {
-  githubIcon.src = "./icons/github_w.png";
-});
+  navigator.clipboard.writeText(emailText).then(() => {
+    notification.classList.add("show");
+
+    // Ocultarla después de 3 segundos (3000ms)
+    setTimeout(() => {
+      notification.classList.remove("show");
+    }, 3000);
+  }).catch(err => {
+    console.error('Error al copiar: ', err);
+  });
+}
